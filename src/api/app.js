56b1d435d90bbe66a05a017cbe451689e15acd82
anders617 <anders617@gmail.com>
2019-10-12 15:14:24 -0400

    ESLint errors

--- src/api/app.js
+++ src/api/app.js
@@ -1,23 +1,23 @@
 // Firebase App (the core Firebase SDK) is always required and must be listed first
-import * as firebase from "firebase/app";
+import * as firebase from 'firebase/app';
 
 // Add the Firebase SDK for Analytics
-import "firebase/analytics";
+import 'firebase/analytics';
 
 // Add the Firebase products
-import "firebase/auth";
-import "firebase/firestore";
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAeu9jttrDSIOA9HrBrv3QsoElQWtHBdvg",
-    authDomain: "bigchat-88c14.firebaseapp.com",
-    databaseURL: "https://bigchat-88c14.firebaseio.com",
-    projectId: "bigchat-88c14",
-    storageBucket: "bigchat-88c14.appspot.com",
-    messagingSenderId: "771086944403",
-    appId: "1:771086944403:web:434d2ceb2565f10cdf2566",
-    measurementId: "G-7HVG4S3150"
+  apiKey: 'AIzaSyAeu9jttrDSIOA9HrBrv3QsoElQWtHBdvg',
+  authDomain: 'bigchat-88c14.firebaseapp.com',
+  databaseURL: 'https://bigchat-88c14.firebaseio.com',
+  projectId: 'bigchat-88c14',
+  storageBucket: 'bigchat-88c14.appspot.com',
+  messagingSenderId: '771086944403',
+  appId: '1:771086944403:web:434d2ceb2565f10cdf2566',
+  measurementId: 'G-7HVG4S3150',
 };
-  
+
 // Initialize Firebase
 export default firebase.initializeApp(firebaseConfig);
--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 // Firebase App (the core Firebase SDK) is always required and must be listed first
-import * as firebase from "firebase/app";
-import "firebase/auth";
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 import app from './app';
 
