69b89bbb6b6fc0531a959a1dfdbcfb9d2533d5af
Ryan Johnston <ryjo1026@gmail.com>
2019-10-12 15:46:03 -0400

    Add some initial folder structure

---
+++ src/components/authentication/AuthenticationContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class AuthenticationContainer extends React.Component {
+  render() {
+    return <div className="AuthenticationContainer"></div>;
+  }
+}
---
+++ src/components/chat/ChatContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default class Chatcontainer extends React.Component {
+    render() {
+        return (
+            <div className="Chatcontainer">
+                
+        </div>);
+    }
+}
