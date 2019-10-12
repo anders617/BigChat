1aef9a97101e1f0cd4ef846e2aeea6e81dd4ab51
Ryan Johnston <ryjo1026@gmail.com>
2019-10-12 15:05:50 -0400

    Fix eslint

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ export default class App extends React.Component {
     super();
     this.chatroom = new ChatRoom('www.netflix.com');
     this.state = {
-      message: "",
-      chatroom: "",
+      message: '',
+      chatroom: '',
       messages: [],
     };
   }
@@ -15,29 +15,51 @@ export default class App extends React.Component {
   setChatRoom = (chatroom) => {
     this.chatroom.stopListening();
     this.chatroom = new ChatRoom(chatroom);
-    this.setState({messages: []});
+    this.setState({ messages: [] });
     this.chatroom.startListening({
-      onChange: ({messages}) => {
-        this.setState({messages});
-      }
+      onChange: ({ messages }) => {
+        this.setState({ messages });
+      },
     });
-  }
+  };
 
   render() {
-    return <div className="App">
-      <div>Hello World</div>
-      <div>
-        <div>Chatroom:</div>
-        <input type="text" onChange={(e) => this.setState({chatroom: e.target.value})}></input>
-        <button onClick={() => this.setChatRoom(this.state.chatroom)} >Set</button>
-        <div>Message:</div>
-        <input type="text" onChange={(e) => this.setState({message: e.target.value})}></input>
-        <button onClick={() => this.chatroom.send({message: this.state.message, userId: 'aboberg'})} >Send</button>
-        <div>Chat Log:</div>
+    const { chatroom, message, messages } = this.state;
+
+    return (
+      <div className="App">
+        <div>Hello World</div>
+        <div>
+          <div>Chatroom:</div>
+          <input
+            type="text"
+            onChange={(e) => this.setState({ chatroom: e.target.value })}
+          />
+          <button type="submit" onClick={() => this.setChatRoom(chatroom)}>
+            Set
+          </button>
+          <div>Message:</div>
+          <input
+            type="text"
+            onChange={(e) => this.setState({ message: e.target.value })}
+          />
+          <button
+            type="submit"
+            onClick={() => this.chatroom.send({ message, userId: 'aboberg' })}
+          >
+            Send
+          </button>
+          <div>Chat Log:</div>
+        </div>
+        <div>
+          {messages.map((msg) => (
+            <div>
+              <br />
+              {msg.message}
+            </div>
+          ))}
+        </div>
       </div>
-      <div>
-        {this.state.messages.map((msg) => (<div><br />{msg.message}</div>))}
-      </div>
-    </div>;
+    );
   }
 }
